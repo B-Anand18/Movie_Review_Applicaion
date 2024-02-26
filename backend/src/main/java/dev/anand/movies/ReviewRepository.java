90760ea23e0ae13863282f1ada994e4da7b87b88
B_Anand <anandbiju000@gmail.com>
2024-02-26 12:17:04 +0530

    first commit

---
+++ backend/src/main/java/dev/anand/movies/ReviewRepository.java
@@ -0,0 +1,14 @@
+package dev.anand.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository  extends MongoRepository<Review, ObjectId> {
+
+
+
+
+
+}
